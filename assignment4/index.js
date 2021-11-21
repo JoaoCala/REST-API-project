3e90959df90b246cc63bb235d4195f398246719f
Joao Figueiredo <kmanycalaverak@hotmail.com>
2021-11-21 14:42:39 +0100

    Added correct version of Rest API assignment

--- assignment4/index.js
+++ assignment4/index.js
@@ -1,47 +1,62 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'localhost',
-      port : 8080,
+      host : '127.0.01',
+      port : 3306,
       user: 'root',
       password: '',
       database:'webDevData'
     }
-});
-
-
-var express = require('express');
-var router = express.Router();
-// var dataConnection = require('db');
-const port = 8080;
-const app = express();
-
-app.get('/', function(req, res, next) {
+  });
+  
+  var express = require('express');
+  // var dataConnection = require('db');
+  const port = 8080;
+  var app = express();
+  app.use(express.json());
+  
+  //read
+  app.get('/read/:city', function(req, res, next) {
      
-    console.log("hi");
-    knex.from('city').select("*")
-    .then((rows) => {
-        for (row of rows) {
-            res.send(`${row['ID']}`);
-        }
-    }).catch((err) => { console.log( err); throw err })
-    .finally(() => {
-        knex.destroy();
-    });
-
-
-    // dbConn.query('SELECT * FROM books ORDER BY id desc',function(err,rows)     {
- 
-    //     if(err) {
-    //         req.flash('error', err);
-    //         // render to views/books/index.ejs
-    //         res.render('books',{data:''});   
-    //     } else {
-    //         // render to views/books/index.ejs
-    //         res.render('books',{data:rows});
-    //     }
-    // });
-});
-
-
-app.listen(port, () => console.log(`listening on port: ${port}`));
+    knex.select('*').from('city').where('Name',req.params.city)
+    .then(data => res.send(data));
+  });
+  
+  //create
+  app.post('/create', async (req, res )=>{
+  
+    let maxID = await knex('city').max('ID', {as : 'max'});
+    console.log(maxID[0].max);
+  
+    let newID = maxID +1;
+  
+    let result = await knex('city').insert({ID: newID}, {Name: req.body.city},
+      {District: req.body.district}, {Population: req.body.population});
+  
+    knex.select('*').from('city').where('Name',req.body.city)
+      .then(data => res.send(data));
+  });
+  
+  //update
+  app.get('/update/:city/:population',async(req, res, )=> {
+     
+    const result = await knex('city').where('Name',req.params.city)
+      .update('Population',req.params.population);
+  
+      knex.select('*').from('city').where('Name',req.params.city)
+      .then(data => res.send(data));
+  });
+  
+  //delete
+  app.get('/delete/:city', async(req, res)=>{
+    let findCity = await knex('city').where('Name', req.params.city);
+  
+    if(findCity==null){
+      res.send(false);
+    }else{
+      knex.from('city').where('Name', req.params.city)
+      .del().then(res.send(true));
+    }
+  });
+  
+  app.listen(port, () => console.log(`Port: ${port}`));
